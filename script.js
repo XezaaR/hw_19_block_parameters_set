706f7d71421b24789f5eb6d7d0d671c31eba9b54
XezaaR <96605840+XezaaR@users.noreply.github.com>
2023-01-25 05:23:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 (()=> {
-    const btn = document.getElementById('btn_show');
-    btn.addEventListener("click", function () {
+    // const btn = document.getElementById('btn_show');
+    // btn.addEventListener("click", function () {
+    //
+    // const width = document.getElementById("input_width").value;
+    // const height = document.getElementById("input_height").value;
+    // const radius = document.getElementById("input_radius").value;
+    // const color = document.getElementById("input_color").value;
+    // const count = document.getElementById("input_count").value;
+    //
+    // const playground = document.getElementById("playground");
+    //
+    // for (let i = 0; i < count; i++) {
+    //     const block = document.createElement("div");
+    //     block.style.width = width + "px";
+    //     block.style.height = height + "px";
+    //     block.style.borderRadius = radius + "px";
+    //     block.style.backgroundColor = color;
+    //     block.style.margin = '20px 0px';
+    //
+    //     const firstChild = playground.firstChild;
+    //     playground.insertBefore(block, firstChild);
+    //     }
+    // });
+    $(document).ready(function(){
+        $("#btn_show").on('click',function(){
+            const width = $("#input_width").val();
+            const height = $("#input_height").val();
+            const radius = $("#input_radius").val();
+            const color = $("#input_color").val();
+            const count = $("#input_count").val();
 
-    const width = document.getElementById("input_width").value;
-    const height = document.getElementById("input_height").value;
-    const radius = document.getElementById("input_radius").value;
-    const color = document.getElementById("input_color").value;
-    const count = document.getElementById("input_count").value;
-
-    const playground = document.getElementById("playground");
-
-    for (let i = 0; i < count; i++) {
-        const block = document.createElement("div");
-        block.style.width = width + "px";
-        block.style.height = height + "px";
-        block.style.borderRadius = radius + "px";
-        block.style.backgroundColor = color;
-        block.style.margin = '20px 0px';
-
-        const firstChild = playground.firstChild;
-        playground.insertBefore(block, firstChild);
-        }
+            for (let i = 0; i < count; i++) {
+                $("#playground").prepend(`<div style="width: ${width}px; height: ${height}px; border-radius: ${radius}px; background-color: ${color}; margin: 20px 0px;"></div>`);
+            }
+        });
     });
+
 })();
